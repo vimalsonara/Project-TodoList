2c9d6fc856d8ffeb5c9df05d98ca86e55e560e56
Vimal Sonara <vvsonara95@gmail.com>
2023-03-05 23:16:13 +0530

    on load mark checked completed todos

--- src/index.js
+++ src/index.js
@@ -25,12 +25,36 @@ todoForm.addEventListener('submit', e => {
 
 // load todos from local storage
 window.addEventListener('load', () => {
+    // asign localstorage to variable
     const storedTodo = JSON.parse(localStorage.getItem('todos'));
+    // check if localstorage todo available
     if(storedTodo != '') {
+        // iterate through all todos
         storedTodo.forEach(element => {
+            // display each local todo to DOM
             todoList.displayTodo(element);
+            // check if todo is completed
+            if (element.completed == true) {
+                // select all todos
+                const todos = document.querySelectorAll('.todo');
+                // iterate through all todos
+                todos.forEach(todo => {
+                    // match localstorage todo & DOM todo
+                    if (todo.childNodes[1].textContent === element.todo) {
+                        // add class to completed todo
+                        todo.childNodes[0].classList.add('markCheck');
+                        // select all completed todo
+                        const markCheck = document.querySelectorAll('.markCheck');
+                        // iterate through all completed todo
+                        markCheck.forEach(check => {
+                            // mark checked all completed todo
+                            check.checked = true;
+                        })
+                    }
+                })
+            }
         });
-    } else {
+       } else {
         return
     }
 })
